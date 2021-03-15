6aafc13827d6d1b1ca8132ab5954e5325941e4d3
Gem Xli <x.li.gem@gmail.com>
2021-03-15 23:04:14 +0800

    feat: add api /posts

--- api/post.go
+++ api/post.go
@@ -3,11 +3,38 @@ package api
 import (
 	"net/http"
 
+	"github.com/cwxyz007/x-cms/entity"
 	"github.com/gin-gonic/gin"
 )
 
-func GetPost(router *gin.RouterGroup) {
-	router.GET("posts", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"ok": "cool"})
+type Pagenation struct {
+	Page   int    `form:"page" binding:"gte=0"`
+	Size   int    `form:"size" binding:"gte=10,lte=100"`
+	SortBy string `form:"sortBy" binding:"oneof=created_at updated_at"`
+}
+
+func GetPosts(router *gin.RouterGroup) {
+	router.GET("/posts", func(c *gin.Context) {
+		pagenation := Pagenation{
+			Page:   0,
+			Size:   10,
+			SortBy: "created_at",
+		}
+
+		if err := c.Bind(&pagenation); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"errMsg": err.Error(),
+			})
+			return
+		}
+
+		db := entity.Db()
+		var posts []entity.Post
+
+		db.Order(pagenation.SortBy + " desc").Offset(pagenation.Page * pagenation.Size).Limit(pagenation.Size).Find(&posts)
+
+		c.JSON(http.StatusOK, gin.H{
+			"posts": posts,
+		})
 	})
 }
--- api/routes.go
+++ api/routes.go
@@ -5,5 +5,5 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(router *gin.Engine) {
 	r := router.Group("/api")
 
-	GetPost(r)
+	GetPosts(r)
 }
